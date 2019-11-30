9dd34f9d58ef8018a8d0ccb95ee4e7669e19e5b4
Kiley Dorton <kileydorton@gmail.com>
2019-11-30 12:59:40 -0500

    Initial commit
    My hopes for this company:
    - it becomes a great place to work, where everyone supports each other and is able to accomplish some of the greatest work of their careers
    - the tools we build help others do great work

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <header>
+        <p>dayowork</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
